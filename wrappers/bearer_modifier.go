6de3854ab154447f5d5b99f8b8d72f64913f79f6
Ramil Aleshkin <rraleshk@mts.ru>
2022-03-25 14:09:08 +0300

    :sparkles: Поправил на работу через запрос

--- wrappers/bearer_modifier.go
+++ wrappers/bearer_modifier.go
@@ -3,18 +3,24 @@ package wrappers
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	log "github.com/sirupsen/logrus"
 )
 
 // SetBearerTokenFromQuery — устанавливаем Bearer токен из хеадера
-func SetBearerTokenFromQuery(req *http.Request) {
+func SetBearerTokenFromQuery(sourceURL string, req *http.Request) error {
 	const (
 		accessTokenKey      = "access_token"
 		authorizationHeader = "Authorization"
 	)
 
-	query := req.URL.Query()
+	sourceRequestURL, parseURLErr := url.Parse(sourceURL)
+	if parseURLErr != nil {
+		return parseURLErr
+	}
+
+	query := sourceRequestURL.Query()
 	at, ok := query[accessTokenKey]
 	if !ok {
 		log.WithFields(log.Fields{"request": req}).Warn("missing access token")
@@ -23,4 +29,6 @@ func SetBearerTokenFromQuery(req *http.Request) {
 		bearerToken := at[0]
 		req.Header.Set(authorizationHeader, fmt.Sprintf("Bearer %s", bearerToken))
 	}
+
+	return nil
 }
